15df1ca3b5d2a46e5778c0336eaf7a3ebd9d1c2d
Jose Francisco Flores Amador <230417@utxicotepec.edu.mx>
2026-01-20 14:10:35 -0600

    main.py
    se agregaron los endpoints de eliminar , buscar y actualizar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from typing import List, Optional
 from uuid import UUID, uuid4
+from fastapi import HTTPException
+from uuid import UUID
 
 from model import Genero, Role, Usuario
 
@@ -32,3 +34,29 @@ async def root():
 @app.get("/api/v1/user")
 async def get_user():
     return db
+
+@app.delete("/api/v1/user/{user_id}")
+async def delete_user(user_id: UUID):
+    for index, user in enumerate(db):
+        if user.id == user_id:
+            db.pop(index)
+            return {"message": "Usuario eliminado correctamente"}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+@app.put("/api/v1/user/{user_id}", response_model=Usuario)
+async def update_user(user_id: UUID, user_update: Usuario):
+    for index, user in enumerate(db):
+        if user.id == user_id:
+            user_update.id = user_id
+            db[index] = user_update
+            return user_update
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+
+
+@app.get("/api/v1/user/{user_id}", response_model=Usuario)
+async def get_user_by_id(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            return user
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
