d28881f05f73979a8e1475bd03416611573ce6e9
Jose Francisco Flores Amador <230417@utxicotepec.edu.mx>
2026-01-20 14:00:24 -0600

    main.py
    cambios en ambos archivos , correciones de escritura

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from typing import List
-from uuid import uuid4
+from typing import List, Optional
+from uuid import UUID, uuid4
 
 from model import Genero, Role, Usuario
 
@@ -20,7 +20,7 @@ db: List[Usuario] = [
         apellidos="Flores Amador",
         genero=Genero.masculino,
         roles=[Role.user]
-    ),
+    )
 ]
 
 
@@ -29,6 +29,6 @@ async def root():
     return {"message": "Hola Mundo"}
 
 
-@app.get("/api/v1/user", response_model=List[Usuario])
-async def get_users():
+@app.get("/api/v1/user")
+async def get_user():
     return db
